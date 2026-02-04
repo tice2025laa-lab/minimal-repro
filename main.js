fcd4ba8a53070329abb1918dbd355d3d30522048
tice2025laa-lab <tice2025.laa@gmail.com>
2026-02-04 14:10:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,71 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let mainWindow;
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        icon: path.join(__dirname, 'build/icons/icon.png'),
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
+        show: false
+    });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Charger votre index.html
+    mainWindow.loadFile('index.html');
+
+    // Afficher quand prêt
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    // Menu personnalisé
+    const template = [
+        {
+            label: 'Fichier',
+            submenu: [
+                {
+                    label: 'Nouveau membre',
+                    accelerator: 'Ctrl+N',
+                    click: () => mainWindow.webContents.send('menu-action', 'new-member')
+                },
+                { type: 'separator' },
+                { label: 'Quitter', role: 'quit' }
+            ]
+        },
+        {
+            label: 'Aide',
+            submenu: [
+                {
+                    label: 'À propos',
+                    click: () => {
+                        dialog.showMessageBox({
+                            type: 'info',
+                            title: 'OLIYMPIUS FITNESS',
+                            message: 'Logiciel de gestion de salle de sport',
+                            detail: 'Développeur: RAKOTONIRINA Rado\nContact: 034 60 761 00',
+                            buttons: ['OK']
+                        });
+                    }
+                }
+            ]
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+app.whenReady().then(createWindow);
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
